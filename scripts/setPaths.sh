81ee8547adb8968cf2c6c234ced1846650d451f2
asogaard <andreas.sogaard@gmail.com>
2016-11-05 11:08:33 +0100

    Updating use of external paths.

--- scripts/setPaths.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-export ARMAPATH=/Users/A/Dropbox/PhD/Work/WaveletML/armadillo-6.500.4
-export HEPMCPATH=/Users/A/Dropbox/hep/HepMC-2.06.09/installation
-
-# --------------------------------------------------------------
-# No need to touch this bit; it just ensures that you don't have
-# to source this script everytime you run in a new shell.
-sed -i.bak s,'\(^ARMAPATH *=\)\(.*\)$',"\1 ${ARMAPATH}",g Makefile
-sed -i.bak s,'\(^HEPMCPATH *=\)\(.*\)$',"\1 ${HEPMCPATH}",g Makefile
