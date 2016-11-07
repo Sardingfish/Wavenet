b4809b1d733c42f2b9c6b3453a3437dcc1943cb0
asogaard <andreas.sogaard@gmail.com>
2016-11-07 18:15:53 +0100

    Add MWE.

---
+++ examples/Example00.cxx
@@ -0,0 +1,28 @@
+// Wavenet include(s).
+#include "Wavenet/Generators.h" /* wavenet::NeedleGenerator */
+#include "Wavenet/Wavenet.h" /* wavenet::Wavenet */
+#include "Wavenet/Coach.h" /* wavevent::Coach */
+
+/**
+ * Example00: Minimal working example. See Example01 for more explanations.
+ */
+int main (int argc, char* argv[]) {
+
+    // Create Generator instance; here NeedleGenerator.
+    wavenet::NeedleGenerator ng;
+    ng.setShape({16,16});
+    
+    // Create Wavenet instance.
+    wavenet::Wavenet wn;
+
+    // Create Coach instance.
+    wavenet::Coach coach ("Example00");
+    coach.setNumCoeffs(2);
+    coach.setGenerator(&ng);
+    coach.setWavenet  (&wn);
+    
+    // Run the training.
+    coach.run();
+
+    return 1;
+}
