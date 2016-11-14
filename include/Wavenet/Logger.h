1022969bbbafa0129a590fbe3456e07394cf5b8c
asogaard <andreas.sogaard@gmail.com>
2016-11-14 16:36:26 +0100

    Ensuring compliance with ATLAS C++ coding guidelines and adding documentation.

--- include/Wavenet/Logger.h
+++ include/Wavenet/Logger.h
@@ -2,9 +2,11 @@
 #define WAVENET_LOGGER_H
 
 /**
- * @file Logger.h
+ * @file   Logger.h
  * @author Andreas Sogaard
- **/
+ * @date   14 November 2016
+ * @brief  Mix-in class providing logging functionality, with instance-specific print levels.
+ */
 
 // STL include(s).
 #include <iostream> /* std::cout, std::left */
@@ -16,62 +18,89 @@
 // Wavenet include(s).
 #include "Wavenet/Type.h"
 
-/* Macros only to be called from classes inheriting from Logger. */
-#define ERROR(...)                  {this->_print(type(*this),  __FUNCTION__, "ERROR",   __VA_ARGS__);}
-#define WARNING(...)                {this->_print(type(*this),  __FUNCTION__, "WARNING", __VA_ARGS__);}
-#define INFO(...)                   {this->_print(type(*this),  __FUNCTION__, "INFO",    __VA_ARGS__);}
-#define DEBUG(...)   if (debug())   {this->_print(type(*this),  __FUNCTION__, "DEBUG",   __VA_ARGS__);}
-#define VERBOSE(...) if (verbose()) {this->_print(type(*this),  __FUNCTION__, "VERBOSE", __VA_ARGS__);}
-/* Macros to be called from wherever. */
-#define FCTERROR(...)               {wavenet::Logger::_fctprint(__FUNCTION__, "ERROR",   __VA_ARGS__);}
-#define FCTWARNING(...)             {wavenet::Logger::_fctprint(__FUNCTION__, "WARNING", __VA_ARGS__);}
-#define FCTINFO(...)                {wavenet::Logger::_fctprint(__FUNCTION__, "INFO",    __VA_ARGS__);}
 
+/// Printing macros, with signature similar to 'printf'.
+// Macros only to be called from classes inheriting from Logger.
+#define ERROR(...)                  {this->print_(type(*this),  __FUNCTION__, "ERROR",   __VA_ARGS__);}
+#define WARNING(...)                {this->print_(type(*this),  __FUNCTION__, "WARNING", __VA_ARGS__);}
+#define INFO(...)                   {this->print_(type(*this),  __FUNCTION__, "INFO",    __VA_ARGS__);}
+#define DEBUG(...)   if (debug())   {this->print_(type(*this),  __FUNCTION__, "DEBUG",   __VA_ARGS__);}
+#define VERBOSE(...) if (verbose()) {this->print_(type(*this),  __FUNCTION__, "VERBOSE", __VA_ARGS__);}
+// Macros to be called from wherever.
+#define FCTERROR(...)               {wavenet::Logger::fctprint_(__FUNCTION__, "ERROR",   __VA_ARGS__);}
+#define FCTWARNING(...)             {wavenet::Logger::fctprint_(__FUNCTION__, "WARNING", __VA_ARGS__);}
+#define FCTINFO(...)                {wavenet::Logger::fctprint_(__FUNCTION__, "INFO",    __VA_ARGS__);}
 
 
 namespace wavenet {
 
+/**
+ * Mix-in class providing logging functionality, with instance-specific print 
+ * levels.
+ *
+ * All classes inheriting from Logger will be able to print nicely formatted 
+ * logging information, at various print levels, using the above macros. In 
+ * addition to 'ERROR', 'WARNING', and 'INFO' levels, which always print, each 
+ * instance inheriting from Logger can be set to print 'DEBUG' and 'VERBOSE' 
+ * statements as well.
+ * 
+ * Any function not called from within an instance inheriting from Logger can 
+ * still print formatted 'ERROR', 'WARNING', and 'INFO' statements using the 
+ * 'FCT...' macros.
+ */
 class Logger {
     
 public:
     
-    // Constructor(s).
+    /// Constructor(s).
     Logger () {};
     
-    // Destructor.
+
+    /// Destructor.
     ~Logger () {};
     
-    // Set method(s).
+
+    /// Set method(s).
     inline void setDebug (const bool& debug = true) {
-        _debug    = debug;
-        _verbose &= debug;
+        m_debug    = debug;
+        m_verbose &= debug;
         return;
     }
     
     inline void setVerbose (const bool& verbose = true) {
-        _verbose  = verbose;
-        _debug   |= verbose;
+        m_verbose  = verbose;
+        m_debug   |= verbose;
         return;
     }
 
-    // Get method(s).
-    inline bool debug   () { return _debug; }
-    inline bool verbose () { return _verbose; }
+
+    /// Get method(s).
+    inline bool debug   () { return m_debug; }
+    inline bool verbose () { return m_verbose; }
+
+
+    /// Public print method(s).
+    // Variadic function, formatting the printed statment (format, ...) using 
+    // the calling non-member function (fct) as well as the print level.
+    static void fctprint_ (std::string fun, std::string level, 
+                           std::string format, ...);
+
+
+protected:
+
+    /// Internal method(s).
+    // Variadic function, formatting the printed statment (format, ...) using 
+    // the calling class (cls) and member function (fct) as well as the print 
+    // level.
+    void print_ (std::string cls, std::string fun, std::string level, 
+                 std::string format, ...);
     
-    // Public print method(s).
-    static void _fctprint (std::string fun, std::string type, std::string format, ...);
 
 protected:
     
-    // Internal method(s).
-    void _print (std::string cls, std::string fun, std::string type, std::string format, ...);
-    
-    
-protected:
-    
-    // Data member(s).
-    bool _debug   = false;
-    bool _verbose = false;
+    /// Data member(s).
+    bool m_debug   = false;
+    bool m_verbose = false;
     
 };
 
