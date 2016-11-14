1567715b27379b47ad260ce5fb9a11b07c184797
asogaard <andreas.sogaard@gmail.com>
2016-11-14 16:34:24 +0100

    Update documentation.

--- include/Wavenet/Type.h
+++ include/Wavenet/Type.h
@@ -2,21 +2,26 @@
 #define WAVENET_TYPE_H
 
 /**
- * @file Type.h
+ * @file   Type.h
  * @author Andreas Sogaard
-**/
+ * @date   14 November 2016
+ * @brief  Utility function to extract demangled class name.
+ */
 
 #include <string> /* std::string */
 #include <typeinfo> /* typeid */
 
 /**
  * Taken from here: http://stackoverflow.com/a/4541470
- **/
+ */
 
 namespace wavenet {
 
+/// Utility functions to extract demangles type name.
+// Demangle the result of built-in 'typeid' function. Compiler dependent. For internal use only.
 std::string demangle(const char* name);
 
+// Return the demangled type name of class 'T' as string. For public use.
 template <class T>
 std::string type(const T& t) {
     return demangle(typeid(t).name());
