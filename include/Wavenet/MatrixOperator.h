35c6479a3691daa9357ecf2f3372af6b67245f96
asogaard <andreas.sogaard@gmail.com>
2016-11-14 16:37:37 +0100

    Optimising operator construction, ensuring compliance with ATLAS C++ coding guidelines and adding documentation.

--- include/Wavenet/MatrixOperator.h
+++ include/Wavenet/MatrixOperator.h
@@ -2,8 +2,10 @@
 #define WAVENET_MATRIXOPERATOR_H
 
 /**
- * @file MatrixOperator.h
+ * @file   MatrixOperator.h
  * @author Andreas Sogaard
+ * @date   14 November 2016
+ * @brief  Mix-in class for matrix operators used in the wavelet/neural net transform.
  */
 
 // STL include(s).
@@ -22,7 +24,7 @@
 namespace wavenet {
 
 /**
- * Abstract class for matrix operators used in the wavelet/neural net transform.
+ * Mix-in class for matrix operators used in the wavelet/neural net transform.
  *
  * This base class handles the construction of the matrix operator (inheriting
  * from arma::Mat) given a set of filter coefficients. The methods for 
@@ -36,54 +38,84 @@ public:
     /// Constructor(s).
     MatrixOperator () {};
     
+
     /// Destructor.
     ~MatrixOperator () {};
     
+
     /// Set method(s).
     inline void setSize (const unsigned& size) {
-        _size = size;
-        if (_filter.n_elem) { setComplete(true); }
+        m_size = size;
+        if (m_filter.n_elem) { setComplete(); }
         return;
     }
-    
-    inline void setComplete (const bool& complete = true) {
-        _complete = complete;
-        return;
-    }
-    
-    /// Get method(s).
-    inline unsigned size () { return _size; }
-    inline bool complete () { return _complete; }
 
-    /// Matrix operator methods.
-    // Purely virtual method for setting the filter coefficients correctly. 
+    // Purely virtual method for setting the filter coefficients correctly. Must 
+    // be implemented by derived classes. 
     virtual void setFilter (const arma::Col<double>& filter) = 0;
     
-    
-public:
-    
-    void construct (); // Main method
-    void _constructByRows ();
-    void _constructByIndices ();
+
+    /// Get method(s).
+    inline unsigned size () const { return m_size; }
+    inline bool complete () const { return m_complete; }
+
+
+    /// Operator construction method(s).
+    // Main interface method. Redirects to appropriate, internal implementation 
+    // method, deducing which method to use for optimal performance.
+    void construct ();
+
+
+protected:
+
+    /// Internal set method(s).
+    // Should be determined by the appropriate set methods, and is therefore not
+    // publicly exposed.
+    inline void setComplete (const bool& complete = true) {
+        m_complete = complete;
+        return;
+    }
+
+
+    /// Internal operator construction method(s).
+    // Construct matrix operator by iterating rows.
+    void constructByRows_ ();
+
+    // Construct matrix operator by iterating filter indices.
+    void constructByIndices_ ();
     
     
 protected:
-    
-    // Data member(s).
-    unsigned _size;
-    arma::Col<double> _filter;
-    bool _complete = false;
-    
+
+    /// Data member(s).
+    // The matrix operator is of size N x 2N, where m_size = log2(N). That is, 
+    // m_size is the number of rows (in powers of 2) of the matrix operator. 
+    unsigned m_size = 0;
+
+    // The set of filter coefficients used to construct the matix operator.
+    arma::Col<double> m_filter = {};
+
+    // Whether the internal configuration of the matrix operator is complete, 
+    // and therefore whether it is ready to be constructed.
+    bool m_complete = false;
+
+
 };
 
 /// Rowshift utility functions, for constructing the matrix operator.
-// Inplace shift the input arma row right by 'shift' steps.
+// Shift the input arma row _in place_ to the right by 'shift' steps.
 void rowshift (arma::Row<double>& row, const int& shift);
 
 // Rowshift, given an arma matrix subview as input.
 inline arma::Row<double> rowshift(const arma::subview_row<double>& row, const int& shift) {
+
+    // Copy the matrix subview to a new row.
     arma::Row<double> newRow = row;
+
+    // Rowshift in place.
     rowshift(newRow, shift);
+
+    // Return the shifted copy.
     return newRow;
 }
 
--- src/MatrixOperator.cxx
+++ src/MatrixOperator.cxx
@@ -4,42 +4,49 @@ namespace wavenet {
     
 void MatrixOperator::construct () {
 
+    // Check whether the matrix operator is properly configured.
     if (!complete()) {
         WARNING("Trying to construct incomplete operator.");
         return;
     }
 
-    // Choose the most efficient (but equivalent) way to construct the matrix operator, given the size and filter configuration
-    const unsigned N = _filter.n_elem;
-    if ((N ==  2 && _size >= 2) ||
-        (N <=  6 && _size >= 3) ||
-        (N <= 20 && _size >= 4) ||
-        (           _size >= 5)) {
-        _constructByIndices();
+    // Choose the most efficient (but equivalent) way to construct the matrix 
+    // operator, given the size and filter configuration. The numbers are found 
+    // empirically, but generally: if we have a large operator and few filter 
+    // coefficients, construct by filter indices; if we have a small operatator 
+    // and many indices, construct by matrix rows.
+    const unsigned N = m_filter.n_elem;
+    if ((N ==  2 && m_size >= 2) ||
+        (N <=  6 && m_size >= 3) ||
+        (N <= 20 && m_size >= 4) ||
+        (           m_size >= 5)) {
+        constructByIndices_();
     } else {
-        _constructByRows();
+        constructByRows_();
     }
 
     return;
 }
 
-void MatrixOperator::_constructByRows () {
+void MatrixOperator::constructByRows_ () {
     
-    const unsigned nRows = (unsigned) pow(2, _size);
-    const unsigned nCols = (unsigned) pow(2, _size + 1);
-    const unsigned N = _filter.n_elem;
+    // Initialise variables.
+    const unsigned nRows = (unsigned) pow(2, m_size);
+    const unsigned nCols = (unsigned) pow(2, m_size + 1);
+    const unsigned N = m_filter.n_elem;
     
     // Initialise the matrix operator to all zeros.
     this->zeros(nRows, nCols);
-    
-    
-    // Initialise single row matrix with the appropriately positioned filter coefficients
+
+    // Initialise single row matrix with the appropriately positioned filter 
+    // coefficients.
     arma::Row<double> v (nCols, arma::fill::zeros);
     for (unsigned i = 0; i < N; i++) {
-        v( (i - N/2 + 1) % nCols ) += _filter(N - i - 1);
+        v( (i - N/2 + 1) % nCols ) += m_filter(N - i - 1);
     }
-    
-    // Use that all rows in the matrix operator are identical modulo even-numbered shifts to construct the remaining rows by shifted versions of the base row.
+    // Use the fact that all rows in the matrix operator are identical modulo 
+    // even-numbered shifts to construct the remaining rows by shifted versions 
+    // of the base row.
     this->row(0) = v;
     for (unsigned i = nRows; i --> 1; ) {
 
@@ -48,38 +55,55 @@ void MatrixOperator::_constructByRows () {
 
         // And set as i'th row in matrix operator.
         this->row(i) = v;        
-
     }
-
     
     return;
 }
 
-void MatrixOperator::_constructByIndices () {
+void MatrixOperator::constructByIndices_ () {
     
-    const unsigned nRows = (unsigned) pow(2, _size);
-    const unsigned nCols = (unsigned) pow(2, _size + 1);
-    const unsigned N = _filter.n_elem;
+    // Initialise variables.
+    const unsigned nRows = (unsigned) pow(2, m_size);
+    const unsigned nCols = (unsigned) pow(2, m_size + 1);
+    const unsigned N = m_filter.n_elem;
     
     // Initialise the matrix operator to all zeros.
     this->zeros(nRows, nCols);
     
+    // Initialise vectors of row- and column indices.
     arma::Col<arma::uword> rows (nRows, arma::fill::zeros);
     arma::Col<arma::uword> cols (nRows, arma::fill::zeros);
 
+    // Row indices are always 0, 1, ... , nRows.
     for (unsigned irow = 0; irow < nRows; irow++) {
         rows(irow) = irow;
     }
+
+    // Column indices and chosed appropriately for the first filter coefficient.
     for (unsigned icol = 0; icol < nRows; icol++) {
         cols(icol) = (N / 2 + 2 * icol) % nCols;
     }
+    
+    // List of combined row- and column indices.
+    arma::Col<arma::uword> indices; 
 
-    arma::Col<arma::uword> indices, idxZero;
+    // List containing the location(s) of indices in the first column, which 
+    // cannot be shifted to the left, since the indices are of type arma::uword
+    // (unsigned).
+    arma::Col<arma::uword> idxZero; 
+
+    // Use the fact that the locations of successive filter coefficients is 
+    // related by a shift to the left by a single column.
     for (unsigned i = 0; i < N; i++) {
-        indices = cols * nRows + rows; 
-        (*this)(indices) += _filter(i);
 
-        // Shift indices.
+        // Compute combined row- and column indices (single unsigned int).
+        indices = cols * nRows + rows; 
+
+        // Add the i'th filter coefficient at these locations.
+        (*this)(indices) += m_filter(i);
+
+        // Shift indices to the left by one column, taking care that we don't 
+        // bring an unsigned number below zero.
         idxZero = find(cols == 0);
         cols(idxZero) += nCols;
         cols -= 1;
@@ -90,26 +114,32 @@ void MatrixOperator::_constructByIndices () {
 
 void rowshift (arma::Row<double>& row, const int& shift) {
     
+    // Initialise length of row vector.
     const unsigned length = row.n_elem;
     
     if (shift > 0 && shift < length) {
-        
+
+        // If shift is in (0, length), perform the shift in place.
         std::rotate(row.begin(), row.begin() + shift, row.end());
         
     } else {
-        
+
+        // If shift is larger than row length, take appropriate modulo and try 
+        // again.
         if (length <  abs(shift)) {
             rowshift(row, sign(shift) * (abs(shift) % length));
         }
-        
+
+        // If shift is negative, add one period (row length) and try again.        
         if (shift  <  0) {
             rowshift(row, length + shift);
         }
         
     }
+
+    // Implicitly: If shift == 0, do nothing.
     
     return;
-    
 }
 
 } // namespace
