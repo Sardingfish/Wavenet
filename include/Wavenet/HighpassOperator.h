6d1ec43fdd17e7c8b99417c8fda79f3fa6e52cdf
asogaard <andreas.sogaard@gmail.com>
2016-11-14 16:38:47 +0100

    Adding check, ensuring compliance with ATLAS C++ coding guidelines and adding documentation.

--- include/Wavenet/HighpassOperator.h
+++ include/Wavenet/HighpassOperator.h
@@ -2,9 +2,11 @@
 #define WAVENET_HIGHPASSOPERATOR_H
 
 /**
- * @file HighpassOperator.h
+ * @file   HighpassOperator.h
  * @author Andreas Sogaard
- **/
+ * @date   14 November 2016 
+ * @brief  Class for high-pass operators used in the wavelet/neural net transform.
+ */
 
 // STL include(s).
 #include <cmath> /* pow */
@@ -18,11 +20,23 @@
 
 namespace wavenet {
 
+/**
+ * Class for high-pass operators used in the wavelet/neural net transform.
+ * 
+ * This derived class implements the mix-in wavenet::MatrixOperator class for 
+ * the case of high-pass wavelet operators. In this case, the setFilter method 
+ * is non-trivial trivial, since the provided filter is assumed to be composed 
+ * of low-pass filter coefficients and therefore the internal filter has to be 
+ * computed from the relation
+ *   b_{k} = (-1)^k a_{N - k - 1}
+ * where {b_{k}} is the set of high-pass filter coefficients, {a_{k}} is the set
+ * of low-pass filter coefficients, and N is the number of such coefficients.
+ */
 class HighpassOperator : public MatrixOperator {
     
 public:
     
-    // Constructor(s).
+    /// Constructor(s).
     HighpassOperator () {};
     
     HighpassOperator (const unsigned& size)
@@ -32,13 +46,17 @@ public:
     { setFilter(filter); };
     
     HighpassOperator (const arma::Col<double>& filter, const unsigned& size)
-    { setSize(size); setFilter(filter); setComplete(true); construct(); };
+    { setSize(size); setFilter(filter); setComplete(); construct(); };
     
-    // Destructor.
+
+    /// Destructor.
     ~HighpassOperator () {};
-    
-    // Matrix operator methods.
-    void setFilter (const arma::Col<double>& filter);
+
+
+    /// Matrix operator methods.
+    // Implementation of virtual method to specify the internal filter from a 
+    // set of low-pass filter coefficients
+    virtual void setFilter (const arma::Col<double>& filter);
     
 };
 
--- src/HighpassOperator.cxx
+++ src/HighpassOperator.cxx
@@ -3,12 +3,23 @@
 namespace wavenet {
     
 void HighpassOperator::setFilter (const arma::Col<double>& filter) {
-    unsigned N = filter.n_elem;
-    _filter.zeros(N);
-    for (unsigned i = 0; i < N; i++) {
-        _filter(i) = pow(-1, i) * filter(N - i - 1);
+    
+    // Check whether number of filter coefficients is even.
+    if (filter.n_elem % 2 != 0) {
+        WARNING("Trying to set odd number of filter coefficients (%d).", filter.n_elem);
+        return;
     }
-    if (_size) { setComplete(true); }
+
+    // Set matrix operator filter from set of low-pass filter coefficients.
+    const unsigned N = filter.n_elem;
+    m_filter.zeros(N);
+    for (unsigned i = 0; i < N; i++) {
+        m_filter(i) = pow(-1, i) * filter(N - i - 1);
+    }
+
+    // If the filter completes the matrix operator, label it so.
+    if (size()) { setComplete(); }
+
     return;
 }
 
