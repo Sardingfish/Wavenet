10e6e663e1b29edbd0daf574e65bbb076ff4de4e
asogaard <andreas.sogaard@gmail.com>
2016-11-15 12:52:20 +0100

    Smart pointers and move semantics, compliance with ATLAS C++ coding guidelines, documentation.

--- include/Wavenet/Generators.h
+++ include/Wavenet/Generators.h
@@ -2,13 +2,17 @@
 #define WAVENET_GENERATORS_H
 
 /**
- * @file Generators.h
+ * @file   Generators.h
  * @author Andreas Sogaard
-**/
+ * @date   14 November 2016
+ * @brief  Various derived generator classes.
+ */
 
 // STL include(s).
-#include <string>
+#include <string> /* std::string */
 #include <algorithm> /* std::max */
+#include <memory> /* std::unique_ptr */
+#include <utility> /* std::move */
 
 #ifdef USE_HEPMC
 // HepMC include(s).
@@ -25,190 +29,180 @@
 
 namespace wavenet {
 
-enum class GeneratorMode {
-   File     = 0,
-   Uniform  = 1,
-   Needle   = 2,
-   Gaussian = 3
-};
-
+/**
+ * Derived class generating needle-like input. 
+ */
 class NeedleGenerator : public GeneratorBase {
-
-    /**
-     * Derived class generating needle-like input. 
-    **/
 
 public:
 
-    // Constructor(s).
+    /// Constructor(s).
     NeedleGenerator () { open(); }
 
-    // Destructor.
+
+    /// Destructor.
     ~NeedleGenerator () {}
 
-    // Method(s).
-    inline const arma::Mat<double>& next () {
-        // Check whether generator is properly initialised.
-        if (!initialised()) {
-            WARNING("Generator not properly initialised.");
-            return _data;
-        }
+
+    /// Generator method(s).
+    virtual inline const arma::Mat<double>& next () {
+
+        // Check whether generator is properly set up.
+        check_();
 
         // Generate next input matrix.
         do {
-            _data.randu();
-            _data.elem( find(_data < 0.995) ) *= 0;
-        } while (arma::accu(_data) == 0);
+            m_data.randu();
+            m_data.elem( find(m_data < 0.995) ) *= 0;
+        } while (arma::accu(m_data) == 0);
 
-        return _data;
+        return m_data;
     }
 
-    inline bool open () { arma::arma_rng::set_seed_random(); return true; }
-    
+    virtual inline bool good () const { return true; }
+
+    virtual inline bool open () { arma::arma_rng::set_seed_random(); return true; }
+  
 };
 
 
+/**
+ * Derived class generating uniformly random input. 
+ */
 class UniformGenerator : public GeneratorBase {
 
-    /**
-     * Derived class generating uniformly random input. 
-    **/
-
 public:
 
-    // Constructor(s).
+    /// Constructor(s).
     UniformGenerator () { open(); }
 
-    // Destructor.
+
+    /// Destructor.
     ~UniformGenerator () {}
 
-    // Method(s).
-    inline const arma::Mat<double>& next () {
-        // Check whether generator is properly initialised.
-        if (!initialised()) {
-            WARNING("Generator not properly initialised.");
-            return _data;
-        }
+
+    /// Generator method(s).
+    virtual inline const arma::Mat<double>& next () {
+
+        // Check whether generator is properly set up.
+        check_();
         
         // Generate next input matrix.
-        _data.randu();
+        m_data.randu();
 
-        return _data;
+        return m_data;
     }
 
-    inline bool open () { arma::arma_rng::set_seed_random(); return true; }
+    virtual inline bool good () const { return true; }
+
+    virtual inline bool open () { arma::arma_rng::set_seed_random(); return true; }
     
 };
 
 
+/**
+ * Derived class generating gaussianly distributed input. 
+*/
 class GaussianGenerator : public GeneratorBase {
 
-    /**
-     * Derived class generating gaussianly distributed input. 
-    **/
 
 public:
 
-    // Constructor(s).
+    /// Constructor(s).
     GaussianGenerator () { open(); }
 
-    // Destructor.
+
+    /// Destructor.
     ~GaussianGenerator () {}
 
-    // Method(s).
-    inline const arma::Mat<double>& next () {
-        // Check whether generator is properly initialised.
-        DEBUG("Entering");
-        if (!initialised()) {
-            ERROR("Generator not properly initialised. Exiting.");
-            return _data;
-        }
 
-        // Generate next input matrix.
-        _data.zeros();
+    /// Generator method(s).
+    virtual inline const arma::Mat<double>& next () {
         
-        const unsigned int sizex = shape()[0];
-        const unsigned int sizey = shape()[1];
+        // Check whether generator is properly set up.
+        check_();
+
+        // Initialise generator input to zeros.
+        m_data.zeros();
         
-        // -- Number of gaussian bumps to overlay
+        // Initialise siez variables.
+        const unsigned sizex = shape()[0];
+        const unsigned sizey = shape()[1];
+        
+        // Get number of gaussian bumps to overlay.
         const unsigned N = 1 + int(arma::as_scalar(arma::randu<arma::mat>(1,1)) * 3); // [1, 2, 3]
         
+        // Generate each bump separately.
         for (unsigned i = 0; i < N; i++) {
             
-            // -- Mean coordinates.
+            // Mean coordinates.
             int mux = int((arma::as_scalar(arma::randu<arma::mat>(1,1)) - 0.5) * (double)sizex);
             int muy = int((arma::as_scalar(arma::randu<arma::mat>(1,1)) - 0.5) * (double)sizey);
 
-            // -- Axis coordinates.
+            // Axis coordinates: Matrices of x- and y-coordinates, resp.
             arma::Mat<double> hx = arma::linspace<arma::mat>(-((double)sizex-1)/2, ((double)sizex-1)/2, sizex);
             arma::Mat<double> hy = arma::linspace<arma::mat>(-((double)sizey-1)/2, ((double)sizey-1)/2, sizey);
             
             hx = repmat(hx, 1, sizey).t();
             hy = repmat(hy, 1, sizex);
 
-            // -- Widths.
+            // Widths.
             double sx = std::max(arma::as_scalar(arma::randn<arma::mat>(1,1))*0.5 + sizex / 4., 2.); 
             double sy = std::max(arma::as_scalar(arma::randn<arma::mat>(1,1))*0.5 + sizey / 4., 2.); 
             arma::Mat<double> gauss = exp( - square(hx - mux) / (2*sq(sx)) - square(hy - muy) / (2*sq(sy)));
             
-            // -- Normalise.
-            gauss /= accu(gauss); // Unit integral.
+            // Normalise to unit height
+            gauss /= accu(gauss);
             gauss *= 1./max(max(gauss));
             
-            _data = _data + gauss.t();
+            // Add to generator input matrix.
+            m_data = m_data + gauss.t();
             
         }
-        DEBUG("Exiting.");
-       return _data;
+        
+        return m_data;
     }
 
-    inline bool open () { arma::arma_rng::set_seed_random(); return true; }
+    virtual inline bool good () const { return true; }
+
+    virtual inline bool open () { arma::arma_rng::set_seed_random(); return true; }
     
 };
 
 
 #ifdef USE_HEPMC
+/**
+ *  Derived class generating input from HepMC files. 
+ */
 class HepMCGenerator : public GeneratorBase {
-
-    /**
-     * Derived class generating input from HepMC files. 
-    **/
-
+    
 public:
 
-    // Constructor(s).
-    //HepMCGenerator () {} // No empty constructor. 'filename' *must* be specified for this generator to make sense.
+    /// Constructor(s).
+    // No empty constructor. 'filename' *must* be specified for this generator to make sense.
+    /* HepMCGenerator () {} */ 
+
     HepMCGenerator (const std::string& filename) { open(filename); }
 
-    // Destructor.
-    ~HepMCGenerator () {
-        if (_event) { delete _event; _event = NULL; }
-        if (_IO)    { delete _IO;    _IO    = NULL; }
-        if (_input) { delete _input; _input = NULL; }
-    }
 
-    // Method(s).
-    inline const arma::Mat<double>& next () {
-        DEBUG("Entering.");
-        // Check whether generator is properly initialised.
-        if (!initialised()) { /* @TODO: To be done externally, for GeneratorBase? */
-            WARNING("Generator not properly initialised.");
-            return _data;
-        }
+    /// Destructor.
+    ~HepMCGenerator () {}
 
-        // Generate next input matrix.
-        _data.zeros();
 
-        if (!good()) { /* @TODO: To be done externally, for GeneratorBase? */
-            ERROR("Reader not good. Exiting.");
-            return _data;
-        }
+    /// Method(s).
+    virtual inline const arma::Mat<double>& next () {
 
-        // -- Fill histogram.
+        // Check whether generator is properly set up.
+        check_();
+
+        // Initialise generator input to zeros.
+        m_data.zeros();
+
+        // Fill histogram.
         TH2F hist ("tmp", "", shape()[0], -3.2, 3.2, shape()[1], -PI, PI);
         
-        HepMC::GenEvent::particle_const_iterator p     = _event->particles_begin();
-        HepMC::GenEvent::particle_const_iterator p_end = _event->particles_end();
+        HepMC::GenEvent::particle_const_iterator p     = m_event->particles_begin();
+        HepMC::GenEvent::particle_const_iterator p_end = m_event->particles_end();
         
 
         for ( ; p != p_end; p++) {
@@ -220,80 +214,77 @@ public:
             hist.Fill((*p)->momentum().eta(), (*p)->momentum().phi(), (*p)->momentum().perp() / 1000.);
         }
 
-        // Fill '_data' matrix with content from 'hist'.
-        DEBUG("Calling HistFillMatrix.");
-        HistFillMatrix(&hist, _data);
-        DEBUG("Done.");
-
-        // -- Get next HepMC event.
-        /**
-         * This is done here, such that when HepMCGenerator::good is called, we're checking the *next* event, and therefore we don't risk generating a bad event.
-        **/
+        // Fill 'm_data' matrix with content from 'hist'.
+        HistFillMatrix(&hist, m_data);
+        
+        // Get next HepMC event.
+        // This is done here, such that when HepMCGenerator::good is called, 
+        // we're checking the *next* event, and therefore we don't risk 
+        // generating a bad event.
         getNextHepMCEvent();
-        DEBUG("Exiting.");
-        return _data;
+        
+        return m_data;
     }
+
+    virtual inline bool good () const { 
+        
+        if (!m_IO)    { INFO("Member object 'm_IO' is nullptr."); }
+        if (!m_event) { INFO("Member object 'm_event' is nullptr."); }
+        
+        return m_IO && m_event; 
+     }
     
-    inline bool open  (const std::string& filename) { 
-        DEBUG("Entering.");
-        _filename = filename;
-        open();
-        DEBUG("Exiting.");
-        return true; 
+    virtual inline bool open  (const std::string& filename) {         
+        m_filename = filename;
+        return open(); 
     }
 
     inline bool open  () { 
-        DEBUG("Entering.");
-        if (_filename == "") {
+
+        // Check whether file name makes sense.
+        if (m_filename == "") {
             ERROR("Filename not set. Exiting.");
             return false;
         }
-        if (!fileExists(_filename)) {
-            ERROR("File '%s' does not exist. Exiting.", _filename.c_str());
+
+        if (!fileExists(m_filename)) {
+            ERROR("File '%s' does not exist. Exiting.", m_filename.c_str());
             return false;
         }
+
+        // Get the HepMC IO objects from input stream of file, from file name.
+        m_input = std::move(std::unique_ptr<std::istream>      (new std::fstream(m_filename.c_str(), std::ios::in)));
+        m_IO    = std::move(std::unique_ptr<HepMC::IO_GenEvent>(new HepMC::IO_GenEvent(*m_input)));
         
-        _input = new std::fstream(_filename.c_str(), std::ios::in);
-        _IO    = new HepMC::IO_GenEvent(*_input);
-        DEBUG("Exiting.");
         return getNextHepMCEvent(); 
     }
 
-    inline bool close () { 
-        DEBUG("Entering.");
-        if (_IO    != NULL) { delete _IO;    _IO    = NULL; }
-        if (_input != NULL) { delete _input; _input = NULL; }
-        DEBUG("Exiting.");
-        return true; 
-    }
-
-    inline bool good  () { 
-        DEBUG("Entering.");
-        if (!_IO)    { INFO("Member object '_IO' is null."); }
-        if (!_event) { INFO("Member object '_event' is null."); }
-        DEBUG("Exiting.");
-        return _IO && _event; 
-     }
-
+    // Read next event from HepMC file.
     inline bool getNextHepMCEvent () {
-        DEBUG("Entering.");
-        if (_event) { delete _event; _event = 0; }
-        if (_IO)    { _event = _IO->read_next_event(); } else {
-            WARNING("Member object '_IO' is null.");
-            DEBUG("Exiting.");
+        
+        if (m_IO)    { m_event = std::move(std::unique_ptr<HepMC::GenEvent>(m_IO->read_next_event())); } else {
+            WARNING("Member object 'm_IO' is nullptr.");
             return false;
         }
-        DEBUG("Exiting.");
+
         return true;
     }
     
+
 private:
 
-    // Data member(s).
-    std::string         _filename = "";
-    std::istream*       _input = nullptr;
-    HepMC::IO_GenEvent* _IO    = nullptr;
-    HepMC::GenEvent*    _event = nullptr;
+    /// Data member(s).
+    // The name of the HepMC from which to generate the input.
+    std::string m_filename = "";
+
+    // Stream used to read the HepMC file.
+    std::unique_ptr<std::istream> m_input = nullptr;
+
+    // HepMC IO object to read HepMC events from file.
+    std::unique_ptr<HepMC::IO_GenEvent> m_IO = nullptr;
+
+    // HepMC event.
+    std::unique_ptr<HepMC::GenEvent> m_event = nullptr;
     
 };
 #endif // USE_HEPMC
